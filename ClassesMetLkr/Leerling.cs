01058e8303f6980e3c6dc32c49898543506665f7
enrick627 <enrick.demunter@student.romerocollege.be>
2020-04-23 14:08:56 +0200

    Add project files.

---
+++ ClassesMetLkr/Leerling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMetLkr
+{
+    class Leerling
+    {   //fields:
+
+        private string famillienaam;
+        //properties
+        public string Voornaam { get; set; }
+
+        public string Familienaam
+        {
+            get { return famillienaam; }
+            set { famillienaam = value; }
+        }
+        public string Naam { get; set; }
+
+        public string famnaam { get; set; }
+        public override string ToString()
+        {
+            return $"{Voornaam.ToLower()} {famillienaam.ToUpper()} ";
+        }
+    }
+}
---
+++ ClassesMetLkr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMetLkr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Leerling leerling;
+            leerling = new Leerling();
+            string Naam, famnaam;
+            Console.WriteLine("geef een voornaam");
+            Naam = Console.ReadLine();
+
+            Console.WriteLine("geef een famillienaam");
+            famnaam = Console.ReadLine();
+
+            leerling.Voornaam = "emma";
+            leerling.Familienaam = "thomson";
+            leerling.Naam = Naam;
+            leerling.famnaam = famnaam;
+            Console.WriteLine($"{leerling.Voornaam} {leerling.Familienaam}");
+            Console.WriteLine($"{leerling.Naam} {leerling.famnaam}");
+            Console.WriteLine(leerling);
+
+            //wachten 
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten");
+            Console.ReadKey();
+
+        }
+    }
+}
